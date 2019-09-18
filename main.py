55a75aab8c661b4db57ea3d3a0953d143ed50376
ryan macbook <rvtsang@gmail.com>
2019-09-17 19:25:58 -0700

    Update tests. Update requirements file with pytz

--- main.py
+++ main.py
@@ -8,17 +8,27 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 
+calendar_module_auth_credentials = None
+
+
 def main():
     assistant = Jiro()
+    # TODO: consider moving this to Jiro or the calendar module
+    initialize_calendar_module()
+    run_tests()
 
-    # Execution loop
+    # execution loop
     while(1):
         sys.stdout.write("> ")
         sys.stdout.flush()
-
         user_input: str = sys.stdin.readline()
 
         assistant.input_handler(user_input)
+
+
+def initialize_calendar_module():
+    global calendar_module_auth_credentials
+    calendar_module_auth_credentials = authenticate_with_calendar_api()
 
 
 class DayAnalytics:
@@ -29,13 +39,50 @@ class DayAnalytics:
         return self.total_committed_time / 3600.0
 
 
-def get_day_analytics(target_date: datetime):
-    auth_credentials = authenticate_with_calendar_api()
+def run_calendar_cmdline():
+    # TODO: do some date parsing
+    target_start_date: datetime = None
+    target_end_date: datetime = None
+
+    # get start date
+    sys.stdout.write("Enter start date: ")
+    sys.stdout.flush()
+    date_user_input: str = sys.stdin.readline().rstrip()
+
+    if date_user_input == "today":
+        target_start_date = datetime.datetime.today()
+    else:
+        try:
+            target_start_date = datetime.datetime.strptime(date_user_input, "%Y-%m-%d")
+        except:
+            print("Unrecognized date... exiting")
+            return
+
+    # get end date
+    sys.stdout.write("Enter end date (press 'Enter' for today): ")
+    sys.stdout.flush()
+    date_user_input: str = sys.stdin.readline().rstrip()
+
+    if date_user_input == "" or date_user_input == "today":
+        target_start_date = datetime.datetime.today()
+    else:
+        try:
+            target_start_date = datetime.datetime.strptime(date_user_input, "%Y-%m-%d")
+        except:
+            print("Unrecognized date... exiting")
+            return
+
+    get_calendar_analytics(target_start_date, target_end_date)
+
+
+def get_calendar_analytics(target_start_date: datetime, target_end_date: datetime = None):
+    if target_end_date == None:
+        target_end_date = target_start_date
 
     # filter for non-all day events
     events = filter(
         lambda event: 'dateTime' in event['start'],
-        get_events_on_day(target_date, auth_credentials)
+        get_events(target_start_date, target_end_date, calendar_module_auth_credentials)
     )
     simplified_events = map(lambda event: event_transformer(event), events)
 
@@ -75,22 +122,31 @@ def authenticate_with_calendar_api():
     return creds
 
 
-def get_events_on_day(target_date_object: datetime, auth_credentials, max_events: int = 100):
+def get_events(
+    target_date_start: datetime, 
+    target_date_end: datetime,
+    auth_credentials, 
+    max_events: int = 100, 
+):
     service = build('calendar', 'v3', credentials=auth_credentials)
+    
+    # If no end date supplied, default to the start date so we fetch the day 
+    if target_date_end == None:
+        target_date_end = target_date_start
 
     date_start = datetime.datetime(
-        target_date_object.year, 
-        target_date_object.month, 
-        target_date_object.day, 
+        target_date_start.year, 
+        target_date_start.month, 
+        target_date_start.day, 
         hour=0, 
         minute=0, 
         second=0, 
         tzinfo=TIME_ZONE
     ).isoformat()
     date_end = datetime.datetime(
-        target_date_object.year, 
-        target_date_object.month, 
-        target_date_object.day, 
+        target_date_end.year, 
+        target_date_end.month, 
+        target_date_end.day, 
         hour=23, 
         minute=59, 
         second=59, 
@@ -101,7 +157,6 @@ def get_events_on_day(target_date_object: datetime, auth_credentials, max_events
         calendarId='primary', 
         timeMin=date_start, 
         timeMax=date_end, 
-        timeZone=PACIFIC_TZ,
         maxResults=10, 
         singleEvents=True, 
         orderBy='startTime',
@@ -129,26 +184,33 @@ def event_transformer(event):
 
 def test_command_line_defaults() -> None:
     assistant = Jiro()
-    assert assistant.get_intent("events today") == EVENTS_TODAY 
+    assert assistant.get_intent("events") == EVENTS 
     assert assistant.get_intent("quit") == QUIT_INTENT
     assert assistant.get_intent("exit") == QUIT_INTENT
     assert assistant.get_intent("test") == RUN_TEST
 
 
-def test_get_events_on_day() -> None:
+# TODO: make this test compatible with travis CI. For now just test locally
+def local_test_get_events() -> None:
     auth_credentials = authenticate_with_calendar_api()
-    test_day_events = get_events_on_day(datetime.datetime(year=2019, month=2, day=3), auth_credentials)
+    test_day = datetime.datetime(year=2019, month=2, day=3)
+    test_day_events = get_events(test_day, test_day, auth_credentials)
 
     assert len(test_day_events) == 2
     assert test_day_events[0]['summary'] == "Test event 1"
     assert test_day_events[1]['summary'] == "Test event 2"
 
 
+def run_tests():
+    print("\nRunning automated tests...")
+    test_command_line_defaults()
+    local_test_get_events()
+    print("All tests passed!\n")
+
 
 class Jiro:
     def __init__(self):
-        print("Jiro Online")
-    
+        return
 
     def input_handler(self, input_string: str) -> None:
         input_string: str = input_string.rstrip()
@@ -159,24 +221,10 @@ class Jiro:
             quit(0)
         elif intent == UNKNOWN_INTENT:
             print("Unrecognized intent")
+        elif intent == EVENTS:
+            run_calendar_cmdline()
         elif intent == RUN_TEST:
             print("running test")
-            # TODO: do some date parsing
-            sys.stdout.write("Enter date: ")
-            sys.stdout.flush()
-            date_user_input: str = sys.stdin.readline().rstrip()
-            target_date: datetime = None
-
-            if date_user_input == "today":
-                target_date = datetime.datetime.today()
-            else:
-                try:
-                    target_date = datetime.datetime.strptime(date_user_input, "%Y-%m-%d")
-                except:
-                    print("Unrecognized date... exiting")
-                    return
-
-            get_day_analytics(target_date)
         else:
             print("Invalid intent state")
 
@@ -189,15 +237,15 @@ class Jiro:
             return QUIT_INTENT 
         elif input_string == "test":
             return RUN_TEST
-        elif input_string == "events today":
-            return EVENTS_TODAY
+        elif input_string == "events":
+            return EVENTS
         else:
             return UNKNOWN_INTENT
     
 
 
 RUN_TEST = "RunTest"
-EVENTS_TODAY = "EventsToday"
+EVENTS = "EventsToday"
 QUIT_INTENT = "Quit"
 UNKNOWN_INTENT = "Unknown"
 PACIFIC_TZ = "US/Pacific"
